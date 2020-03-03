a7585218d455c927705645cafe22a179a149e612
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 16:22:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)# code an argument here
   dwarves.each.with_index { |dwarf, i| puts "#{i + 1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)# code an argument here
+  calls.map { |call| "#{call.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
